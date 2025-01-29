39ea22d746e897d9042bbeb1fa57dea2df217342
mac <mac@macs-MacBook-Pro.local>
2025-01-29 19:22:48 +0900

    feat: article 레이아웃 구현
    Desc
    - article 컴포넌트 레이아웃 구현 완료
    - 더미 데이터 활용하여 article 컴포넌트 배치 및 전체 레이아웃 관련 속성 수정 및 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,112 @@
+import { Article } from '@/components/article';
+
+interface DummyType {
+  title: string;
+  company: string;
+  date: string;
+}
+
+const dummy: Array<DummyType> = [
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+  {
+    title: '신문 기사 제목입니다.',
+    company: '매일 경제',
+    date: '2025.01.29 (수)',
+  },
+];
+
 export default function Home() {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <ul className="article-list">
+        {dummy.map((info, index) => (
+          <Article {...info} key={index} />
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -1,3 +1,24 @@
-export const Article = () => {
-  return null;
+import { Bookmark } from './Bookmark';
+import './article.css';
+
+interface ArticleProps {
+  title: string;
+  company: string;
+  date: string;
+}
+
+export const Article = (props: ArticleProps) => {
+  const { title, company, date } = props;
+
+  return (
+    <li className="article-container">
+      <h6>{title}</h6>
+      <div className="article-info">
+        <span>{company}</span>
+        <span>{date}</span>
+      </div>
+
+      <Bookmark />
+    </li>
+  );
 };
