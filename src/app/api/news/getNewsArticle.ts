8211b8880b2709beb074666d6376eefc072f7bfb
kimyh93 <kimyh93@rexgen.co.kr>
2025-02-09 21:53:48 +0900

    fix: 뉴스 기사 관련 api 라우터 로직 수정
    Desc
    - 기존 로직에 오류 및 비효율이 존재하여 전체적인 로직 수정
    - 네이버에서 받아온 html parsing 수행 시, 뉴스 페이지 목록 관련 부분도 parsing 하여 전체 페이지 수 파악
    - redis 활용하여 기사 목록 및 전체 페이지 수 캐싱 처리하여 활용
    - 네이버 뉴스에서 뉴스 기사 페이지네이션 관련하여 다소 독특하게 처리하는 부분이 존재하여 엣지 케이스 처리 (utils.news.ts에 주석 첨가하여 처리 로지 구현)

--- src/app/api/news/getNewsArticle.ts
+++ src/app/api/news/getNewsArticle.ts
@@ -2,23 +2,20 @@ import iconv from 'iconv-lite';
 import { redisService } from '@/lib/redisService';
 import { parsingHtml } from './parsingHtml';
 
-export const getNewsArticle = async (url: string, page: string | null) => {
-  /** 쿼리 스트링 값이 page=2 이상일 때만 첨부하여 호출 */
-  const requestUrlWithQueryString =
-    !page || page === '1' ? url : `${url}&page=${page}`;
+export const getNewsArticle = async (date: string, page: string | null) => {
+  const baseUrl = `https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&oid=009&listType=paper&date=${date}`;
+  const requestUrl = page ? `${baseUrl}&page=${page}` : baseUrl;
 
   /** redis에 저장해놓은 데이터가 존재할 경우 */
-  const caching = await redisService.get(requestUrlWithQueryString);
+  const caching = await redisService.get(requestUrl);
 
   if (caching) {
-    return {
-      nextPageRedisKey: requestUrlWithQueryString,
-      articles: JSON.parse(caching),
-    };
+    const { articles, totalPageNum } = JSON.parse(caching);
+    return { articles, totalPageNum };
   }
 
   /** caching 데이터가 존재하지 않을 경우 */
-  const response = await fetch(requestUrlWithQueryString);
+  const response = await fetch(requestUrl);
 
   if (!response.ok) {
     throw new Error(
@@ -28,9 +25,9 @@ export const getNewsArticle = async (url: string, page: string | null) => {
 
   const buffer = await response.arrayBuffer();
   const html = iconv.decode(Buffer.from(buffer), 'EUC-KR'); // UTF-8 방식으로 디코딩
-  const articles = parsingHtml(html); // 기사 제목, 주소, 신문사, 날짜 데이터 parsing
+  const { articles, totalPageNum } = await parsingHtml(html, date); // 뉴스 기사 목록, 전체 페이지 수 parsing
 
   // redis에 caching 처리 후 반환
-  redisService.set(requestUrlWithQueryString, JSON.stringify(articles));
-  return { nextPageRedisKey: requestUrlWithQueryString, articles };
+  redisService.set(requestUrl, JSON.stringify({ articles, totalPageNum }));
+  return { articles, totalPageNum };
 };
---
+++ src/app/api/news/utils.parsing.ts
@@ -0,0 +1,61 @@
+import { HTMLElement } from 'node-html-parser';
+import { redisService } from '@/lib/redisService';
+
+/** 기사 관련 HTML을 parsing 하는 API */
+export const parsingArticle = (root: HTMLElement) => {
+  // 기시 목록이 담긴 div 탐색 API (class = list_body.newsflash_body)
+  const mainContent = root.querySelector('div.list_body.newsflash_body');
+
+  if (!mainContent) {
+    console.error("❌ 'list_body.newsflash_body' 요소를 찾을 수 없습니다.");
+    return [];
+  }
+
+  const articles: {
+    title: string; // 기사 제목
+    url: string; // 주소
+    writing: string; // 신문사
+    date: string; // 일자
+  }[] = [];
+
+  mainContent.querySelectorAll('ul li dl').forEach((dtElement) => {
+    const writing =
+      dtElement.querySelector('span.writing')?.textContent.trim() || '';
+    const date = dtElement.querySelector('span.date')?.textContent.trim() || '';
+
+    // dt 내부의 a 태그 탐색 후 title, url 추출
+    const articleLink = dtElement.querySelector('a');
+    if (articleLink) {
+      const title = articleLink.text.trim() || '';
+      const url = articleLink.getAttribute('href') || '';
+
+      if (title && url) {
+        articles.push({ title, url, writing, date });
+      }
+    }
+  });
+
+  return articles;
+};
+
+/** 기사 전체 페이지 수 관련  HTML을 parsing 하는 API */
+export const parsingTotalPageNum = async (root: HTMLElement, date: string) => {
+  const totalPageNumKey = `${date}_TOTAL_PAGE_NUM`;
+
+  // caching 된 값이 있는지 확인
+  const cachedTotalPageNum = await redisService.get(totalPageNumKey);
+  if (cachedTotalPageNum) return JSON.parse(cachedTotalPageNum);
+
+  // 없을 경우 parsing
+  const pageList = root.querySelector('div.topbox_type6');
+
+  if (!pageList) {
+    console.error("❌ 'topbox_type6' 요소를 찾을 수 없습니다.");
+    return null;
+  }
+
+  // caching 처리 후 전체 페이지 개수 반환
+  const totalPageNum = pageList.querySelectorAll('a').length;
+  redisService.set(totalPageNumKey, JSON.stringify(totalPageNum));
+  return totalPageNum;
+};
